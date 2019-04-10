3b331eaafb86877f8a274f8da72ca020d82d8313
DevMai90 <dmai90@yahoo.com>
2019-04-09 22:50:26 -0700

    Created new post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose'); // Because we are dealing with the database
+const passport = require('passport'); // Will have protected routes
 
 // @route   GET api/posts/test
 // @desc    Tests users route
 // @access  Public
 router.get('/test', (req, res) =>
   res.json({ msg: 'Posts route handler works' })
+);
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const newPost = new Post({
+      test: req.body.text,
+      name: req.body.name,
+      avatar: req.body.name,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
 );
 
 module.exports = router;

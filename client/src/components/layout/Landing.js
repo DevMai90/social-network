a7a682403bc705ed1b85d3ca6df1eb7d7a756b15
DevMai90 <dmai90@yahoo.com>
2019-04-13 00:36:25 -0700

    Configured page routes

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
   render() {
@@ -13,12 +14,12 @@ class Landing extends Component {
                   Create a developer profile/portfolio, share posts and get help
                   from other developers
                 </p>
-                <a href="/" className="btn btn-lg btn-info mr-2">
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
                   Sign Up
-                </a>
-                <a href="/" className="btn btn-lg btn-light">
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
                   Login
-                </a>
+                </Link>
               </div>
             </div>
           </div>

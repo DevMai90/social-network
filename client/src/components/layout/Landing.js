f9552fcca7be9b243b5d80e596e636fbda28deb0
DevMai90 <dmai90@yahoo.com>
2019-04-12 23:39:43 -0700

    Configure Landing page component

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-white">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Developer Connector</h1>
+                <p className="lead">
+                  Create a developer profile/portfolio, share posts and get help
+                  from other developers
+                </p>
+                <a href="/" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </a>
+                <a href="/" className="btn btn-lg btn-light">
+                  Login
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;

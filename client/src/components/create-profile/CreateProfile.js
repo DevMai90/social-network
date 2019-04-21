d68d3d3bea810f95ead4c9793569345bc9049d93
DevMai90 <dmai90@yahoo.com>
2019-04-21 07:06:43 -0700

    Imported text input field components into CreateProfile component

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -24,7 +24,46 @@ class CreateProfile extends Component {
     instagram: '',
     errors: {}
   };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    console.log('Submit');
+  };
+
   render() {
+    const {
+      errors,
+      handle,
+      status,
+      company,
+      website,
+      location,
+      skills,
+      githubusername,
+      bio,
+      twitter,
+      facebook,
+      linkedin,
+      youtube,
+      istagram
+    } = this.state;
+
+    // Select options for status
+    const options = [
+      { label: '* Select Professional Status', value: 0 },
+      { label: 'Developer', value: 'Developer' },
+      { label: 'Junior Developer', value: 'Junior Developer' },
+      { label: 'Senior Developer', value: 'Senior Developer' },
+      { label: 'Manager', value: 'Manager' },
+      { label: 'Student or Learning', value: 'Student or Learning' },
+      { label: 'Instructor or Teacher', value: 'Instructor or Teacher' },
+      { label: 'Intern', value: 'Intern' },
+      { label: 'Other', value: 'Other' }
+    ];
+
     return (
       <div className="create-profile">
         <div className="container">
@@ -35,6 +74,79 @@ class CreateProfile extends Component {
                 Let's get some information to make your profile stand out
               </p>
               <small className="d-block pb-3">* = required fields</small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="* Profile Handle"
+                  name="handle"
+                  value={handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info="A unique handle for your profile URL. Your full name, company name, and nickname"
+                />
+
+                <SelectListGroup
+                  name="status"
+                  value={status}
+                  onChange={this.onChange}
+                  options={options}
+                  error={errors.status}
+                  info="Give us an idea of where you are at in your career"
+                />
+
+                <TextFieldGroup
+                  placeholder="Company"
+                  name="company"
+                  value={company}
+                  onChange={this.onChange}
+                  error={errors.company}
+                  info="Could be your own company or one you work for"
+                />
+
+                <TextFieldGroup
+                  placeholder="website"
+                  name="website"
+                  value={website}
+                  onChange={this.onChange}
+                  error={errors.website}
+                  info="Could be your own website or company one"
+                />
+
+                <TextFieldGroup
+                  placeholder="Location"
+                  name="location"
+                  value={location}
+                  onChange={this.onChange}
+                  error={errors.location}
+                  info="City and state suggested (Eg. San Jose, CA)"
+                />
+
+                <TextFieldGroup
+                  placeholder="Skills"
+                  name="skills"
+                  value={skills}
+                  onChange={this.onChange}
+                  error={errors.skills}
+                  info="Please use comma separated values (Eg. HTML,CSS,JavaScript,PHP)"
+                />
+
+                <TextFieldGroup
+                  placeholder="Github Username"
+                  name="githubusername"
+                  value={githubusername}
+                  onChange={this.onChange}
+                  error={errors.githubusername}
+                  info="If you want your latest repos and a Github link, include your username"
+                />
+
+                <TextAreaFieldGroup
+                  placeholder="A short bio about yourself"
+                  name="bio"
+                  value={bio}
+                  onChange={this.onChange}
+                  error={errors.bio}
+                  info="Tell us a little about yourself"
+                />
+              </form>
             </div>
           </div>
         </div>
